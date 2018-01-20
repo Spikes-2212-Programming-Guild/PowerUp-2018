8da48637715ad1a17d9f2167fe146cd79db9d4d1
Matangn13 <matangn13@gmail.com>
2018-01-20 19:23:35 +0200

    Added components of lift to SubsystemComponents and to RobotMap

--- PowerUp-2018/src/com/spikes2212/robot/RobotMap.java
+++ PowerUp-2018/src/com/spikes2212/robot/RobotMap.java
@@ -14,21 +14,26 @@ package com.spikes2212.robot;
  * floating around.
  */
 public class RobotMap {
-	public static interface CAN{
+	public static interface CAN {
 		public static final int CLIMBER = 0;
 		public static final int CLAW = 4;
 	}
-	
-	public static interface PWM{
+
+	public static interface PWM {
 		public static final int FOLDER = 1;
 		public static final int ROLLER = 6;
+		public static final int LIFT_MOTOR_A = 8;
+		public static final int LIFT_MOTOR_B = 9;
 	}
-	
-	public static interface DIO{
+
+	public static interface DIO {
 		public static final int FOLDER_MAX_LIMIT = 2;
 		public static final int FOLDER_MIN_LIMIT = 3;
 		public static final int CLAW_LIMIT = 5;
 		public static final int ROLLER_LIGHT_SENSOR = 7;
+		public static final int LIFT_HALL_EFFECTS = 9;
+		public static final int LIFT_LIMIT_UP = 10;
+		public static final int LIFT_LIMIT_DOWN = 11;
 	}
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
--- PowerUp-2018/src/com/spikes2212/robot/SubsystemComponents.java
+++ PowerUp-2018/src/com/spikes2212/robot/SubsystemComponents.java
@@ -10,20 +10,28 @@ public class SubsystemComponents {
 	public static interface Climber {
 		public static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(RobotMap.CAN.CLIMBER);
 	}
-	
-	public static interface Folder{
+
+	public static interface Folder {
 		public static final VictorSP MOTOR = new VictorSP(RobotMap.PWM.FOLDER);
 		public static final DigitalInput MAX_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MAX_LIMIT);
-		public static final DigitalInput MIN_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MIN_LIMIT); 
+		public static final DigitalInput MIN_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MIN_LIMIT);
 	}
-	
-	public static interface Claw{
+
+	public static interface Claw {
 		public static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(RobotMap.CAN.CLAW);
 		public static final DigitalInput LIMIT = new DigitalInput(RobotMap.DIO.CLAW_LIMIT);
 	}
-	
-	public static interface Roller{
+
+	public static interface Roller {
 		public static final VictorSP MOTOR = new VictorSP(RobotMap.PWM.ROLLER);
 		public static final DigitalInput LIGHT_SENSOR = new DigitalInput(RobotMap.DIO.ROLLER_LIGHT_SENSOR);
+	}
+
+	public static interface Lift {
+		public static final VictorSP MOTOR_A = new VictorSP(RobotMap.PWM.LIFT_MOTOR_A);
+		public static final VictorSP MOTOR_B = new VictorSP(RobotMap.PWM.LIFT_MOTOR_B);
+		public static final DigitalInput HALL_EFFECTS = new DigitalInput(RobotMap.DIO.LIFT_HALL_EFFECTS);
+		public static final DigitalInput LIMIT_UP = new DigitalInput(RobotMap.DIO.LIFT_LIMIT_UP);
+		public static final DigitalInput LIMIT_DOWN = new DigitalInput(RobotMap.DIO.LIFT_LIMIT_DOWN);
 	}
 }
