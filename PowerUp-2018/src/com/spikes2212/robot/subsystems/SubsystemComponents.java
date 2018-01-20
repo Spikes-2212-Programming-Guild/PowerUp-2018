f80190af54749a1941f48547aee5d62c56d37027
Matangn13 <matangn13@gmail.com>
2018-01-20 15:44:38 +0200

    Changed names of components (removed the name of the system they are in from the name)

--- PowerUp-2018/src/com/spikes2212/robot/subsystems/SubsystemComponents.java
+++ PowerUp-2018/src/com/spikes2212/robot/subsystems/SubsystemComponents.java
@@ -9,17 +9,17 @@ import edu.wpi.first.wpilibj.VictorSP;
 public class SubsystemComponents {
 
 	public static interface Climber {
-		public static final WPI_TalonSRX CLIMBER_MOTOR = new WPI_TalonSRX(RobotMap.CAN.CLIMBER);
+		public static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(RobotMap.CAN.CLIMBER);
 	}
 	
 	public static interface Folder{
-		public static final VictorSP FOLDER_MOTOR = new VictorSP(RobotMap.PWM.FOLDER);
-		public static final DigitalInput FOLDER_MAX_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MAX_LIMIT);
-		public static final DigitalInput FOLDER_MIN_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MIN_LIMIT); 
+		public static final VictorSP MOTOR = new VictorSP(RobotMap.PWM.FOLDER);
+		public static final DigitalInput MAX_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MAX_LIMIT);
+		public static final DigitalInput MIN_LIMIT = new DigitalInput(RobotMap.DIO.FOLDER_MIN_LIMIT); 
 	}
 	
 	public static interface Claw{
-		public static final WPI_TalonSRX CLAW_MOTOR = new WPI_TalonSRX(RobotMap.CAN.CLAW);
-		public static final DigitalInput CLAW_LIMIT = new DigitalInput(RobotMap.DIO.CLAW_LIMIT);
+		public static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(RobotMap.CAN.CLAW);
+		public static final DigitalInput LIMIT = new DigitalInput(RobotMap.DIO.CLAW_LIMIT);
 	}
 }
