9123f61b0ee5525d94934150d0188240d526af18
Matangn13 <matangn13@gmail.com>
2018-01-20 15:45:38 +0200

    Changed name of constants (removed name of subsystem from the name)

--- PowerUp-2018/src/com/spikes2212/robot/subsystems/SubsystemConstants.java
+++ PowerUp-2018/src/com/spikes2212/robot/subsystems/SubsystemConstants.java
@@ -6,10 +6,10 @@ import com.spikes2212.dashboard.ConstantHandler;
 
 public class SubsystemConstants {
 	public static interface Climber {
-		public static final Supplier<Double> CLIMBER_MAX_VOLTAGE = ConstantHandler.addConstantDouble("Climber Max Voltage", 25);
+		public static final Supplier<Double> MAX_VOLTAGE = ConstantHandler.addConstantDouble("Climber Max Voltage", 25);
 	}
 	
 	public static interface Claw {
-		public static final Supplier<Double> CLAW_MAX_VOLTAGE = ConstantHandler.addConstantDouble("Claw Max Voltage", 10);
+		public static final Supplier<Double> MAX_VOLTAGE = ConstantHandler.addConstantDouble("Claw Max Voltage", 10);
 	}
 }
