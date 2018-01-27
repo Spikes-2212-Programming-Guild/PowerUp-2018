5638dfa67c2397feca6fafe21a7d25aecbe3696b
Itamar Rivkind <Michorn313@gmail.com>
2018-01-27 12:26:02 +0200

    Changed hall effects to normally closed

--- PowerUp-2018/src/com/spikes2212/robot/Commands/MoveLift.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/MoveLift.java
@@ -24,11 +24,11 @@ public class MoveLift extends MoveBasicSubsystem {
 		super.execute();
 		if (SubsystemComponents.Lift.LIMIT_UP.get())
 			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 4 : 3.5;
-		else if (SubsystemComponents.Lift.HALL_EFFECTS_MID_SCALE.get())
+		else if (!SubsystemComponents.Lift.HALL_EFFECTS_MID_SCALE.get())
 			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 3.5 : 2.5;
-		else if (SubsystemComponents.Lift.HALL_EFFECTS_LOW_SCALE.get())
+		else if (!SubsystemComponents.Lift.HALL_EFFECTS_LOW_SCALE.get())
 			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 2.5 : 1.5;
-		else if (SubsystemComponents.Lift.HALL_EFFECTS_SWITCH.get())
+		else if (!SubsystemComponents.Lift.HALL_EFFECTS_SWITCH.get())
 			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 1.5 : 0.5;
 		else if (SubsystemComponents.Lift.LIMIT_DOWN.get())
 			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 0.5 : 0;
