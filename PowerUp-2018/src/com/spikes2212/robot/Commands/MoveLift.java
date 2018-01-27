dde01b55327873019b3e0b9d7374df9e1b56b30f
Itamar Rivkind <Michorn313@gmail.com>
2018-01-27 12:17:55 +0200

    Added MoveLift

---
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/MoveLift.java
@@ -0,0 +1,38 @@
+package com.spikes2212.robot.Commands;
+
+import java.util.function.Supplier;
+
+import com.spikes2212.genericsubsystems.BasicSubsystem;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemToTarget;
+import com.spikes2212.robot.Robot;
+import com.spikes2212.robot.SubsystemComponents;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveLift extends MoveBasicSubsystem {
+
+	public MoveLift(Supplier<Double> speed) {
+		super(Robot.lift, speed);
+	}
+
+	@Override
+	protected void execute() {
+		super.execute();
+		if (SubsystemComponents.Lift.LIMIT_UP.get())
+			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 4 : 3.5;
+		else if (SubsystemComponents.Lift.HALL_EFFECTS_MID_SCALE.get())
+			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 3.5 : 2.5;
+		else if (SubsystemComponents.Lift.HALL_EFFECTS_LOW_SCALE.get())
+			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 2.5 : 1.5;
+		else if (SubsystemComponents.Lift.HALL_EFFECTS_SWITCH.get())
+			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 1.5 : 0.5;
+		else if (SubsystemComponents.Lift.LIMIT_DOWN.get())
+			SubsystemComponents.Lift.position = (speedSupplier.get() > 0) ? 0.5 : 0;
+
+	}
+
+}
