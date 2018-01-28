52d243150ccffb2f5353c537d64a9fefa60d96d3
Itamar Rivkind <Michorn313@gmail.com>
2018-01-28 20:19:43 +0200

    Corrected PickUpCube

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PickUpCube.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PickUpCube.java
@@ -12,8 +12,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class PickUpCube extends CommandGroup {
 
     public PickUpCube() {
-    	addSequential(new MoveBasicSubsystem(Robot.roller, SubsystemConstants.Roller.TAKE_SPEED));
+    	addParallel(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
+    	addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.OPEN_SPEED));
+    	addSequential(new MoveBasicSubsystem(Robot.roller, SubsystemConstants.Roller.IN_SPEED));
     	addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.CLOSE_SPEED));
-    	addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.UP_SPEED));
     }
 }
