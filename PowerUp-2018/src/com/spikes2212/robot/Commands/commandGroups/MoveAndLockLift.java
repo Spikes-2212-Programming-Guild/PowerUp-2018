827c41005907469846e28a1bf53dbbcf609114fe
Itamar Rivkind <Michorn313@gmail.com>
2018-01-28 21:29:47 +0200

    Changed moveUp to a speed Supplier

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/MoveAndLockLift.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/MoveAndLockLift.java
@@ -1,5 +1,7 @@
 package com.spikes2212.robot.Commands.commandGroups;
 
+import java.util.function.Supplier;
+
 import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
 import com.spikes2212.robot.Robot;
 import com.spikes2212.robot.SubsystemConstants;
@@ -13,9 +15,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MoveAndLockLift extends CommandGroup {
 
 	//The variable moveUp tells the command group if the lift should move up or down
-	public MoveAndLockLift(boolean moveUp) {
+	public MoveAndLockLift(Supplier<Double> speed) {
 		addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.UNLOCK_SPEED));
-		addSequential(new MoveLift((moveUp) ? SubsystemConstants.Lift.UP_SPEED : SubsystemConstants.Lift.DOWN_SPEED));
+		addSequential(new MoveLift( speed));
 		addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
 	}
 }
