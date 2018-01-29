7298e158c63c8fed3826715e94f2eb98feb8a16f
Matangn13 <matangn13@gmail.com>
2018-01-29 20:18:33 +0200

    Added PickUpCube command group

---
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PickUpCube.java
@@ -0,0 +1,19 @@
+package com.spikes2212.robot.Commands.commandGroups;
+
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.robot.Robot;
+import com.spikes2212.robot.SubsystemConstants;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class PickUpCube extends CommandGroup {
+
+	public PickUpCube() {
+		addSequential(new MoveBasicSubsystem(Robot.roller, SubsystemConstants.Roller.ROLL_IN_SPEED));
+		addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.CLOSE_SPEED));
+		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.UP_SPEED));
+	}
+}
