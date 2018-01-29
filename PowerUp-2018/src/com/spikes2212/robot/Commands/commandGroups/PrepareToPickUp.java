a3a56a5d75e64ff903e544c1d87590548de822a8
Matangn13 <matangn13@gmail.com>
2018-01-29 21:10:17 +0200

    Added documentation to PrepareToPickUp

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PrepareToPickUp.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PrepareToPickUp.java
@@ -7,13 +7,17 @@ import com.spikes2212.robot.SubsystemConstants;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *
+ * This command group prepares the robot so it will be ready to pick up a cube at any time
+ *@author Matan
  */
 public class PrepareToPickUp extends CommandGroup {
 
     public PrepareToPickUp() {
+    	//moving the lift down to the ground in the level of the cubes
     	addSequential(new MoveAndLockLift(SubsystemConstants.Lift.DOWN_SPEED));
+    	//moving the folder down so it can hold a cube
     	addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
+    	//making sure the claw is open so the cube can get in the folder
     	addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.OPEN_SPEED));
     }
 }
