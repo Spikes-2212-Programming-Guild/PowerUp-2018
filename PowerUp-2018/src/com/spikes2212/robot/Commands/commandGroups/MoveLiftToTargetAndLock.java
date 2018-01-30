ae422308dd9c49bfbcf5845f1cbfdf3356e007bf
omri cohen <omrichoen@gmail.com>
2018-01-30 18:16:43 +0200

    Created MoveLiftToTargetAndLock

---
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/MoveLiftToTargetAndLock.java
@@ -0,0 +1,21 @@
+package com.spikes2212.robot.Commands.commandGroups;
+
+import java.util.function.Supplier;
+
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.robot.Robot;
+import com.spikes2212.robot.SubsystemConstants;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class MoveLiftToTargetAndLock extends CommandGroup {
+
+    public MoveLiftToTargetAndLock(Supplier<Double> speed, Supplier<Boolean> onTarget) {
+      addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.UNLOCK_SPEED));
+      addSequential(new MoveBasicSubsystemToTarget(Robot.lift,speed,onTarget));
+      addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
+    }
+}
