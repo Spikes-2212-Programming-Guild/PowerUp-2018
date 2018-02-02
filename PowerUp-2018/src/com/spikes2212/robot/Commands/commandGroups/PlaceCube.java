a88137deb8d1467e82d2ac45a9ae4690be277637
Itamar Rivkind <Michorn313@gmail.com>
2018-02-02 12:00:14 +0200

    Added docs to explain the comment

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class PlaceCube extends CommandGroup {
 
     public PlaceCube() {
+    	//TODO check if we need to also roll the cube out
         addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
         addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.OPEN_SPEED));
         //addSequential(new MoveBasicSubsystemWithTimeSinceReachingLimit(Robot.roller, SubsystemConstants.Roller.ROLL_OUT_SPEED, SubsystemConstants.Roller.WAIT_TIME.get()));
