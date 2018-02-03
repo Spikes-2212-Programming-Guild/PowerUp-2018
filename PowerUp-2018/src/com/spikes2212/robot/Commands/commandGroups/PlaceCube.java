344ccea2051489da6fec05c89d6f240dc770f5a3
Spikes-2212 <spikes.no2212@gmail.com>
2018-02-03 13:52:36 +0200

    Changed roller movement to toTarget

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
@@ -1,8 +1,9 @@
 package com.spikes2212.robot.Commands.commandGroups;
 
 import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
-import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemWithTimeSinceReachingLimit;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemToTarget;
 import com.spikes2212.robot.Robot;
+import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -12,12 +13,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class PlaceCube extends CommandGroup {
 
-    public PlaceCube() {
-    	//TODO check if we need to also roll the cube out
-        addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
-        addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.OPEN_SPEED));
-        //addSequential(new MoveBasicSubsystemWithTimeSinceReachingLimit(Robot.roller, SubsystemConstants.Roller.ROLL_OUT_SPEED, SubsystemConstants.Roller.WAIT_TIME.get()));
-        addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.UP_SPEED));
-        
-    }
+	public PlaceCube() {
+		// TODO check if we need to also roll the cube out
+		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
+		addSequential(new MoveBasicSubsystem(Robot.claw, SubsystemConstants.Claw.OPEN_SPEED));
+		/*addSequential(new MoveBasicSubsystemToTarget(Robot.roller, SubsystemConstants.Roller.ROLL_OUT_SPEED,
+				() -> SubsystemComponents.Roller.LIGHT_SENSOR.get()));*/
+		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.UP_SPEED));
+
+	}
 }
