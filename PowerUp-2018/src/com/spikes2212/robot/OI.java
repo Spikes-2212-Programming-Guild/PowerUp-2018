110ebb867e59f0acd108daf2ce9d3d61ac014449
Itamar Rivkind <Michorn313@gmail.com>
2018-02-08 19:21:47 +0200

    Created navigator buttons and triggers

--- PowerUp-2018/src/com/spikes2212/robot/OI.java
+++ PowerUp-2018/src/com/spikes2212/robot/OI.java
@@ -7,7 +7,10 @@
 
 package com.spikes2212.robot;
 
+import com.spikes2212.utils.XboXUID;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -16,7 +19,30 @@ import edu.wpi.first.wpilibj.Joystick;
 public class OI /* GEVALD */ {
 	private Joystick driverLeft = new Joystick(0);
 	private Joystick driverRight = new Joystick(1);
+	
+	private XboXUID navigator = new XboXUID(2);
+	
+	
+	//navigator
+	private JoystickButton liftSwitchButton;
+	private JoystickButton liftLowScaleButton;
+	private JoystickButton liftMidScaleButton;
+	private JoystickButton placeCubeButton;
+	private JoystickButton prepareToPickCubeButton;
+	private JoystickButton pickUpCubeButton;
+	
+	public OI() {
+		
+	}
 
+	public double getLiftUp() {
+		return navigator.getRTAxis();
+	}
+	
+	public double getLiftDown() {
+		return navigator.getLTAxis();
+	}
+	
 	public double getForward() {
 		return driverRight.getY();
 	}
@@ -24,31 +50,5 @@ public class OI /* GEVALD */ {
 	public double getRotation() {
 		return driverLeft.getX();
 	}
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
-
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
-
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
-
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
-
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
-
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
+	
 }
