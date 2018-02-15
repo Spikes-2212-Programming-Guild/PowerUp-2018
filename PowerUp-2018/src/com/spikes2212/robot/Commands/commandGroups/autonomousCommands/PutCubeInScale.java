f4f2fb4c8acf334880e167b353cf7bb52d419613
uriahrokach <uriahrokach@gmail.com>
2018-02-15 19:03:55 +0200

    Added Commands for other subsystems

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/PutCubeInScale.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/PutCubeInScale.java
@@ -3,11 +3,13 @@ package com.spikes2212.robot.Commands.commandGroups.autonomousCommands;
 import java.util.function.Supplier;
 
 import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcadeWithPID;
 import com.spikes2212.robot.Robot;
 import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
+import com.spikes2212.robot.Commands.commandGroups.PlaceCube;
 import com.spikes2212.utils.PIDSettings;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -19,6 +21,8 @@ public class PutCubeInScale extends CommandGroup {
 
 	public static final Supplier<Double> FORWARD_SPEED = ConstantHandler
 			.addConstantDouble("ScoreScaleFromSide - Forward Speed", 0.4);
+	public static final Supplier<Double> ROTATE_SPEED = ConstantHandler
+			.addConstantDouble("ScoreScaleFromSide - Rotate Speed", 0.4);
 	public static final Supplier<Double> MOVING_KP = ConstantHandler.addConstantDouble("ScoreScaleFromSide - Moving kp",
 			0.7);
 	public static final Supplier<Double> MOVING_KI = ConstantHandler.addConstantDouble("ScoreScaleFromSide - Moving ki",
@@ -37,6 +41,9 @@ public class PutCubeInScale extends CommandGroup {
 				DISTANCE_FROM_SCALE, FORWARD_SPEED,
 				new PIDSettings(MOVING_KP.get(), MOVING_KI.get(), MOVING_KD.get(), TOLERANCE.get(), WAIT_TIME.get()),
 				2.0));
-
+		addSequential(new DriveArcade(Robot.drivetrain, ()->0.0, ROTATE_SPEED));
+		addSequential(new MoveBasicSubsystem(Robot.lift, SubsystemConstants.Lift.UP_SPEED));
+		addSequential(new PlaceCube());
+		addSequential(new MoveBasicSubsystem(Robot.lift, SubsystemConstants.Lift.DOWN_SPEED));
 	}
 }
