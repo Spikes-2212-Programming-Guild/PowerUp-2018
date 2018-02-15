09fa14e214a7071d71a390e10ac2772773108db4
Itamar Rivkind <Michorn313@gmail.com>
2018-02-15 19:11:14 +0200

    Updated CommandGroups

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/MoveLift.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/MoveLift.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
 import com.spikes2212.robot.Robot;
+import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -15,7 +16,7 @@ public class MoveLift extends CommandGroup {
 
 	public MoveLift(Supplier<Double> speed) {
 		addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.UNLOCK_SPEED));
+		addParallel(new MoveBasicSubsystem(Robot.liftLocker, () -> SubsystemComponents.Lift.LIMIT_UP.get() ? SubsystemConstants.LiftLocker.LOCK_SPEED.get() : 0.0));
 		addSequential(new MoveBasicSubsystem(Robot.lift, speed));
-		addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
 	}
 }
--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
@@ -16,8 +16,8 @@ public class PlaceCube extends CommandGroup {
 	public PlaceCube() {
 		// TODO check if we need to also roll the cube out
 		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
-		/*addSequential(new MoveBasicSubsystemToTarget(Robot.roller, SubsystemConstants.Roller.ROLL_OUT_SPEED,
-				() -> SubsystemComponents.Roller.LIGHT_SENSOR.get()));*/
+		addSequential(new MoveBasicSubsystemToTarget(Robot.roller, SubsystemConstants.Roller.ROLL_OUT_SPEED,
+				() -> SubsystemConstants.Roller.LASER_SENSOR_CONSTANT.get()/SubsystemComponents.Roller.LASER_SENSOR.getVoltage()>SubsystemConstants.Roller.CUBE_DISTANCE.get()));
 		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.UP_SPEED));
 
 	}
