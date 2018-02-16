9ee9e27186709547e23ebcdaaf86de3d724c4a93
Itamar Rivkind <Michorn313@gmail.com>
2018-02-16 13:16:33 +0200

    Added pick side choice to DriveStraightAndPutCubeInSwitchAuto

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/DriveStraightAndPutCubeInSwitchAuto.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/DriveStraightAndPutCubeInSwitchAuto.java
@@ -21,23 +21,29 @@ public class DriveStraightAndPutCubeInSwitchAuto extends CommandGroup {
 
 	public static final Supplier<Double> FORWARD_SPEED = ConstantHandler
 			.addConstantDouble("ScoreScaleFromSide - Forward Speed", 0.4);
-	public static final Supplier<Double> KP = ConstantHandler.addConstantDouble("ScoreScaleFromSide - kp", 0.7);
-	public static final Supplier<Double> KI = ConstantHandler.addConstantDouble("ScoreScaleFromSide - ki", 0.04);
-	public static final Supplier<Double> KD = ConstantHandler.addConstantDouble("ScoreScaleFromSide - kd", 0.1);
-	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("ScoreScaleFromSide - Tolerance",
+	public static final Supplier<Double> KP = ConstantHandler.addConstantDouble("DriveStraightAndPutCubeInSwitch - kp", 0.7);
+	public static final Supplier<Double> KI = ConstantHandler.addConstantDouble("DriveStraightAndPutCubeInSwitch - ki", 0.04);
+	public static final Supplier<Double> KD = ConstantHandler.addConstantDouble("DriveStraightAndPutCubeInSwitch - kd", 0.1);
+	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("DriveStraightAndPutCubeInSwitch - Tolerance",
 			0.5);
 	public static final Supplier<Double> MOVING_WAIT_TIME = ConstantHandler
-			.addConstantDouble("ScoreScaleFromSide - Wait Time", 0.5);
+			.addConstantDouble("DriveStraightAndPutCubeInSwitch - Wait Time", 0.5);
 	public static final Supplier<Double> DISTANCE_FROM_SWITCH = ConstantHandler
-			.addConstantDouble("ScoreScaleFromSide - Distance From Scale", 0.4);
+			.addConstantDouble("DriveStraightAndPutCubeInSwitch - Distance From Switch", 0.4);
 	
-    public DriveStraightAndPutCubeInSwitchAuto() {
-    	addSequential(new DriveTankWithPID(Robot.drivetrain, SubsystemComponents.Drivetrain.LEFT_ENCODER, SubsystemComponents.Drivetrain.RIGHT_ENCODER,
-				DISTANCE_FROM_SWITCH, 
-				new PIDSettings(KP.get(), KI.get(), KD.get(), TOLERANCE.get(), MOVING_WAIT_TIME.get())));
-    	addSequential(new MoveLiftToTarget(SubsystemComponents.Lift.HallEffects.SWITCH));
-		addSequential(new PlaceCube());
-		addSequential(new MoveLift(SubsystemConstants.Lift.DOWN_SPEED));
-    	
+    public DriveStraightAndPutCubeInSwitchAuto(char startSide) {
+    	if(Robot.gameData.charAt(0) == startSide) {
+    		addSequential(new DriveTankWithPID(Robot.drivetrain, SubsystemComponents.Drivetrain.LEFT_ENCODER, SubsystemComponents.Drivetrain.RIGHT_ENCODER,
+    				DISTANCE_FROM_SWITCH, 
+    				new PIDSettings(KP.get(), KI.get(), KD.get(), TOLERANCE.get(), MOVING_WAIT_TIME.get())));
+    		addSequential(new MoveLiftToTarget(SubsystemComponents.Lift.HallEffects.SWITCH));
+    		addSequential(new PlaceCube());
+    		addSequential(new MoveLift(SubsystemConstants.Lift.DOWN_SPEED));
+    	}
+    	else
+    		addSequential(new DriveTankWithPID(Robot.drivetrain, SubsystemComponents.Drivetrain.LEFT_ENCODER, SubsystemComponents.Drivetrain.RIGHT_ENCODER,
+    				DISTANCE_FROM_SWITCH, 
+    				new PIDSettings(KP.get(), KI.get(), KD.get(), TOLERANCE.get(), MOVING_WAIT_TIME.get())));
+    		
     }
 }
