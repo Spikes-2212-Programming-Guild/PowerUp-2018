6c6ea7fe6b89eaf088daabc1f0e7b53fb00a9983
Tomer Sitton <tomer.sitton1@gmail.com>
2018-02-18 21:01:35 +0200

    fixed folder movement in command groups

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/PlaceCube.java
@@ -14,8 +14,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class PlaceCube extends CommandGroup {
 
 	public PlaceCube() {
-		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED));
-		addSequential(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.STAYING_SPEED));
+		addParallel(new MoveBasicSubsystem(Robot.folder, () -> SubsystemComponents.Folder.MIN_LIMIT.get()
+				? SubsystemConstants.Folder.STAYING_SPEED.get() : SubsystemConstants.Folder.DOWN_SPEED.get()));
 		addSequential(new MoveBasicSubsystemToTarget(Robot.roller, SubsystemConstants.Roller.ROLL_OUT_SPEED,
 				() -> SubsystemComponents.Roller.hasCube()));
 
