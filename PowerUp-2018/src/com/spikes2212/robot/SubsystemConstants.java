10759d9d8a54e1fa09aa5e5511f60a7d15af718c
Spikes-2212 <spikes.no2212@gmail.com>
2018-02-19 06:12:20 +0200

    killed claw

--- PowerUp-2018/src/com/spikes2212/robot/SubsystemConstants.java
+++ PowerUp-2018/src/com/spikes2212/robot/SubsystemConstants.java
@@ -21,13 +21,6 @@ public class SubsystemConstants {
 		public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("Climber Speed", 0.9);
 	}
 
-	public static interface Claw {
-		public static final Supplier<Double> MAX_VOLTAGE = ConstantHandler.addConstantDouble("Claw Max Voltage", 25);
-
-		public static final Supplier<Double> OPEN_SPEED = ConstantHandler.addConstantDouble("Claw Open Speed", 0.5);
-		public static final Supplier<Double> CLOSE_SPEED = ConstantHandler.addConstantDouble("Claw Close Speed", -0.6);
-	}
-
 	public static interface Folder {
 		public static final Supplier<Double> UP_SPEED = ConstantHandler.addConstantDouble("Folder Up Speed", 0.5);
 		public static final Supplier<Double> DOWN_SPEED = ConstantHandler.addConstantDouble("Folder Down Speed", -0.6);
