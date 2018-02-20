0962d78e045933dba29fb1bfc734aca24e0d075c
Spikes-2212 <spikes.no2212@gmail.com>
2018-02-21 00:17:30 +0200

    added forward movement to ScoreSwitchFromSideAuto.java

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/ScoreSwitchFromSideAuto.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/ScoreSwitchFromSideAuto.java
@@ -4,8 +4,10 @@ import java.util.function.Supplier;
 
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 import com.spikes2212.robot.Robot;
 import com.spikes2212.robot.SubsystemComponents;
+import com.spikes2212.robot.SubsystemConstants;
 import com.spikes2212.robot.Commands.commandGroups.MoveLiftToTarget;
 import com.spikes2212.robot.Commands.commandGroups.PlaceCube;
 
@@ -18,6 +20,10 @@ public class ScoreSwitchFromSideAuto extends CommandGroup {
 			.addConstantDouble("score switch from side auto - turning speed", 168);
 	public static final Supplier<Double> TURNING_TIME_OUT = ConstantHandler
 			.addConstantDouble("score switch from side auto - turning timeout", 168);
+	public static final Supplier<Double> FORWARD_SPEED = ConstantHandler
+			.addConstantDouble("score switch from side auto - forward speed", 168);
+	public static final Supplier<Double> FORWARD_TIME_OUT = ConstantHandler
+			.addConstantDouble("score switch from side auto - forward timeout", 168);
 
 	public ScoreSwitchFromSideAuto(char startSide) {
 
@@ -30,6 +36,7 @@ public class ScoreSwitchFromSideAuto extends CommandGroup {
 
 		// moving the lift to the right height and placing the cube
 		addSequential(new MoveLiftToTarget(SubsystemComponents.Lift.HallEffects.SWITCH));
+		addSequential(new DriveArcade(Robot.drivetrain, FORWARD_SPEED, () -> 0.0), FORWARD_TIME_OUT.get());
 		addSequential(new PlaceCube());
 	}
 }
