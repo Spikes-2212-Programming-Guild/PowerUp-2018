689642d1e3c2838a9ae3b19dfd00a7e1ad87be1d
noam mantin <noam mantin@gmail.com>
2018-02-20 16:52:31 +0200

    fixed MiddleToSwitchAuto

--- PowerUp-2018/src/com/spikes2212/robot/Robot.java
+++ PowerUp-2018/src/com/spikes2212/robot/Robot.java
@@ -57,8 +57,8 @@ public class Robot extends TimedRobot {
 	public static boolean waitForData = true;
 
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
@@ -95,8 +95,9 @@ public class Robot extends TimedRobot {
 
 		drivetrain.setDefaultCommand(new DriveArcade(drivetrain, oi::getForward, oi::getRotation));
 
-		lift.setDefaultCommand(new MoveBasicSubsystem(lift, () -> SubsystemComponents.LiftLocker.LIMIT_LOCKED.get()
-				? 0.0 : SubsystemConstants.Lift.STAYING_SPEED.get()));
+		lift.setDefaultCommand(
+				new MoveBasicSubsystem(lift, () -> SubsystemComponents.LiftLocker.LIMIT_LOCKED.get() ? 0.0
+						: SubsystemConstants.Lift.STAYING_SPEED.get()));
 
 		liftLocker.setDefaultCommand(new MoveBasicSubsystem(liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
 
@@ -198,9 +199,9 @@ public class Robot extends TimedRobot {
 	}
 
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
@@ -223,7 +224,7 @@ public class Robot extends TimedRobot {
 
 			switch (autoChooser.getSelected()) {
 			case "switch from middle":
-				autoCommand = new MiddleToSwitchAuto();
+				autoCommand = new MiddleToSwitchAuto(gameData);
 				break;
 			case "switch from side":
 				if (side != 'n') {
@@ -249,15 +250,15 @@ public class Robot extends TimedRobot {
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the
+	 * auto name from the text box below the Gyro
 	 *
 	 * <p>
 	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * chooser code above (like the commented example) or additional comparisons to
+	 * the switch structure below with additional strings & commands.
 	 */
 	@Override
 	public void autonomousInit() {
