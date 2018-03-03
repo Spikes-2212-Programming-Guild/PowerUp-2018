1aa943543ee5fe71da462c68b0df5ff9bd81ecbd
Spikes-2212 <spikes.no2212@gmail.com>
2018-03-03 18:07:45 +0200

    added folder down button

--- PowerUp-2018/src/com/spikes2212/robot/OI.java
+++ PowerUp-2018/src/com/spikes2212/robot/OI.java
@@ -8,6 +8,7 @@
 package com.spikes2212.robot;
 
 import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemWithTimeSinceReachingLimit;
 import com.spikes2212.robot.Commands.commandGroups.MoveLift;
 import com.spikes2212.robot.Commands.commandGroups.MoveLiftToTarget;
 import com.spikes2212.robot.Commands.commandGroups.PickUpCube;
@@ -36,6 +37,7 @@ public class OI /* GEVALD */ {
 	private Button pickUpCube;
 	private Button stop;
 	private Button folderUp;
+	private Button folderDown;
 	private Button liftUp;
 	private Button liftDown;
 
@@ -50,6 +52,7 @@ public class OI /* GEVALD */ {
 		placeCube = navigator.getUpButton();
 		pickUpCube = navigator.getLeftButton();
 		folderUp = navigator.getLBButton();
+		folderDown = navigator.getDownButton();
 		liftUp = navigator.getRBButton();
 		liftDown = navigator.getRightStickButton();
 		stop = navigator.getLeftStickButton();
@@ -66,6 +69,7 @@ public class OI /* GEVALD */ {
 				() -> SubsystemComponents.Lift.getPosition() < SubsystemComponents.Lift.HallEffects.SWITCH.getIndex()
 						? SubsystemConstants.Lift.SECOND_DOWN_SPEED.get()
 						: SubsystemConstants.Lift.FIRST_DOWN_SPEED.get()));
+		folderDown.whenPressed(new MoveBasicSubsystem(Robot.folder, SubsystemConstants.Folder.DOWN_SPEED_SUPPLIER));
 	}
 
 	public double getForward() {
