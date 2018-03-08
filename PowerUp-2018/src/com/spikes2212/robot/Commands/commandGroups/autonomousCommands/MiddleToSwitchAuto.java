5aeca009522f7a4f117196cca6eaa346e8c1d118
Itamar Rivkind <Michorn313@gmail.com>
2018-03-08 18:17:53 +0200

    organized classes in auto package

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/MiddleToSwitchAuto.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/MiddleToSwitchAuto.java
@@ -1,11 +1,18 @@
 package com.spikes2212.robot.Commands.commandGroups.autonomousCommands;
 
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcadeWithPID;
+import com.spikes2212.robot.ImageProcessingConstants;
 import com.spikes2212.robot.Robot;
 import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
 import com.spikes2212.robot.Commands.commandGroups.MoveLiftToTarget;
 import com.spikes2212.robot.Commands.commandGroups.PlaceCube;
+import com.spikes2212.utils.PIDSettings;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -17,15 +24,54 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class MiddleToSwitchAuto extends CommandGroup {
 
+	public static final Supplier<Double> FORWARD_SPEED = ConstantHandler
+			.addConstantDouble("switch from middle auto - forward speed", 0.4);
+	public static final Supplier<Double> ROTATION_SPEED = ConstantHandler
+			.addConstantDouble("switch from middle auto - rotation speed", 0.6);
+	public static final Supplier<Double> ROTATION_TIME = ConstantHandler
+			.addConstantDouble("switch from middle auto  - rotation time", 0.5);
+
+	// orienting constants
+	public static final Supplier<Double> TOLERANCE = ConstantHandler
+			.addConstantDouble("switch from middle auto - orienting tolerance", 0);
+	public static final Supplier<Double> PID_WAIT_TIME = ConstantHandler
+			.addConstantDouble("switch from middle auto PID waitTime", 1);
+	public static final Supplier<Double> ORIENTATION_FORWARD_SPEED = ConstantHandler
+			.addConstantDouble("switch from middle auto - orientation forward speed", 0.45);
+	public static final Supplier<Double> ORIENTATION_TIME_OUT = ConstantHandler
+			.addConstantDouble("switch from middle auto - oriantation timeout", 3.8);
+
 	public MiddleToSwitchAuto(String gameData) {
 		addParallel(new DriveToSwitchFromMiddle(gameData));
 		// move lift
 		addSequential(new MoveLiftToTarget(SubsystemComponents.Lift.HallEffects.SWITCH));
 		addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
 	}
+
 	@Override
 	protected void end() {
 		super.end();
 		new PlaceCube().start();
+	}
+
+	public class DriveToSwitchFromMiddle extends CommandGroup {
+		// initial turning constants
+
+		public DriveToSwitchFromMiddle(String gameData) {
+			// turn to the correct direction
+			addSequential(
+					new DriveArcade(Robot.drivetrain, FORWARD_SPEED,
+							() -> gameData.charAt(0) == 'L' ? ROTATION_SPEED.get() : -ROTATION_SPEED.get()),
+					ROTATION_TIME.get());
+
+			// drive while orienting
+			addSequential(new DriveArcadeWithPID(Robot.drivetrain, ImageProcessingConstants.CENTER, () -> 0.0,
+					ORIENTATION_FORWARD_SPEED,
+					new PIDSettings(SubsystemConstants.Drivetrain.ORIENTATION_KP.get(),
+							SubsystemConstants.Drivetrain.ORIENTATION_KI.get(),
+							SubsystemConstants.Drivetrain.ORIENTATION_KD.get(), TOLERANCE.get(), PID_WAIT_TIME.get()),
+					ImageProcessingConstants.RANGE), ORIENTATION_TIME_OUT.get());
+		}
+
 	}
 }
--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/StraightToSwitchAuto.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/StraightToSwitchAuto.java
@@ -3,40 +3,33 @@ package com.spikes2212.robot.Commands.commandGroups.autonomousCommands;
 import java.util.function.Supplier;
 
 import com.spikes2212.dashboard.ConstantHandler;
-import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcadeWithPID;
 import com.spikes2212.robot.ImageProcessingConstants;
 import com.spikes2212.robot.Robot;
+import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
+import com.spikes2212.robot.Commands.commandGroups.MoveLiftToTarget;
+import com.spikes2212.robot.Commands.commandGroups.PlaceCube;
 import com.spikes2212.utils.PIDSettings;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class DriveToSwitchFromMiddle extends CommandGroup {
-	// initial turning constants
-	public static final Supplier<Double> FORWARD_SPEED = ConstantHandler
-			.addConstantDouble("switch from middle auto - forward speed", 0.4);
-	public static final Supplier<Double> ROTATION_SPEED = ConstantHandler
-			.addConstantDouble("switch from middle auto - rotation speed", 0.6);
-	public static final Supplier<Double> ROTATION_TIME = ConstantHandler
-			.addConstantDouble("switch from middle auto  - rotation time", 0.5);
-
+public class StraightToSwitchAuto extends CommandGroup {
 	// orienting constants
 	public static final Supplier<Double> TOLERANCE = ConstantHandler
-			.addConstantDouble("switch from middle auto - orienting tolerance", 0);
+			.addConstantDouble("score switch straight auto - orienting tolerance", 0);
 	public static final Supplier<Double> PID_WAIT_TIME = ConstantHandler
-			.addConstantDouble("switch from middle auto PID waitTime", 1);
+			.addConstantDouble("score switch straight  auto PID waitTime", 1);
 	public static final Supplier<Double> ORIENTATION_FORWARD_SPEED = ConstantHandler
-			.addConstantDouble("switch from middle auto - orientation forward speed", 0.45);
+			.addConstantDouble("score switch straight - orientation forward speed", 0.5);
 	public static final Supplier<Double> ORIENTATION_TIME_OUT = ConstantHandler
-			.addConstantDouble("switch from middle auto - oriantation timeout", 3.8);
+			.addConstantDouble("score switch straight - oriantation timeout", 2.3);
 
-	public DriveToSwitchFromMiddle(String gameData) {
-		// turn to the correct direction
-		addSequential(
-				new DriveArcade(Robot.drivetrain, FORWARD_SPEED,
-						() -> gameData.charAt(0) == 'L' ? ROTATION_SPEED.get() : -ROTATION_SPEED.get()),
-				ROTATION_TIME.get());
+	public StraightToSwitchAuto() {
+		// move lift
+		addSequential(new MoveLiftToTarget(SubsystemComponents.Lift.HallEffects.SWITCH));
+		addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
 
 		// drive while orienting
 		addSequential(new DriveArcadeWithPID(Robot.drivetrain, ImageProcessingConstants.CENTER, () -> 0.0,
@@ -45,5 +38,7 @@ public class DriveToSwitchFromMiddle extends CommandGroup {
 						SubsystemConstants.Drivetrain.ORIENTATION_KI.get(),
 						SubsystemConstants.Drivetrain.ORIENTATION_KD.get(), TOLERANCE.get(), PID_WAIT_TIME.get()),
 				ImageProcessingConstants.RANGE), ORIENTATION_TIME_OUT.get());
+		// place cube
+		addSequential(new PlaceCube());
 	}
 }
