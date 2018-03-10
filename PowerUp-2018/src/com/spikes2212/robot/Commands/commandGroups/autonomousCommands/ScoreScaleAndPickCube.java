4aa901b9522054f8e37667977ad2aa89a5d0d529
Tomer Sitton <tomer.sitton1@gmail.com>
2018-03-10 10:43:42 +0200

    added score scale

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/ScoreScaleAndPickCube.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/ScoreScaleAndPickCube.java
@@ -3,16 +3,13 @@ package com.spikes2212.robot.Commands.commandGroups.autonomousCommands;
 import java.util.function.Supplier;
 
 import com.spikes2212.dashboard.ConstantHandler;
-import com.spikes2212.genericsubsystems.commands.MoveBasicSubsystem;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcadeWithPID;
 import com.spikes2212.robot.ImageProcessingConstants;
 import com.spikes2212.robot.Robot;
 import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
-import com.spikes2212.robot.Commands.commandGroups.MoveLift;
 import com.spikes2212.robot.Commands.commandGroups.PickUpCube;
-import com.spikes2212.robot.Commands.commandGroups.PlaceCube;
 import com.spikes2212.utils.PIDSettings;
 import com.spikes2212.utils.RunnableCommand;
 
@@ -57,60 +54,25 @@ public class ScoreScaleAndPickCube extends CommandGroup {
 		addSequential(new RunnableCommand(
 				() -> ImageProcessingConstants.NETWORK_TABLE.getEntry("pipeline").setString(powerCubePipelineName)));
 
-		// the correct side
-		if (gameData.charAt(1) == startSide) {
-			// driving to the correct scale set point
-			addSequential(new MoveToSetpointWithEncoders(BETWEEN_SWITCH_AND_SCALE));
+		// score scale
+		addSequential(new ScoreScaleAuto(gameData, startSide));
 
-			// rotate 90 degrees
-			addSequential(
-					new DriveArcade(Robot.drivetrain, () -> 0.0,
-							() -> startSide == 'L' ? -TURNING_SPEED.get() : TURNING_SPEED.get()),
-					TURNING_TIME_OUT.get());
+		// rotate 180 degrees
+		addSequential(
+				new DriveArcade(Robot.drivetrain, () -> 0.0,
+						() -> startSide == 'L' ? TURNING_SPEED.get() : -TURNING_SPEED.get()),
+				TURNING_TIME_OUT.get() * 2);
 
-			// drive to the correct x position of the scale
-			addSequential(new MoveToSetpointWithEncoders(CLOSE_SCALE_X_SET_POINT));
+		// initiate picking cube sequence
+		addParallel(new PickUpCube());
 
-			// rotate -90 degrees
-			addSequential(
-					new DriveArcade(Robot.drivetrain, () -> 0.0,
-							() -> startSide == 'L' ? TURNING_SPEED.get() : -TURNING_SPEED.get()),
-					TURNING_TIME_OUT.get());
+		// chase the cube
+		addSequential(new DriveArcadeWithPID(Robot.drivetrain, ImageProcessingConstants.CENTER, () -> 0.0,
+				ORIENTATION_FORWARD_SPEED, () -> SubsystemComponents.Roller.hasCube(),
+				new PIDSettings(SubsystemConstants.Drivetrain.ORIENTATION_KP.get(),
+						SubsystemConstants.Drivetrain.ORIENTATION_KI.get(),
+						SubsystemConstants.Drivetrain.ORIENTATION_KD.get(), TOLERANCE.get(), PID_WAIT_TIME.get()),
+				ImageProcessingConstants.RANGE));
 
-			// move lift up
-			addSequential(new MoveLift(SubsystemConstants.Lift.UP_SPEED));
-			addSequential(new MoveBasicSubsystem(Robot.liftLocker, SubsystemConstants.LiftLocker.LOCK_SPEED));
-
-			// drive a little bit straight
-			addSequential(new DriveArcade(Robot.drivetrain, FORWARD_SPEED, () -> 0.0), FORWARD_TIME_OUT.get());
-
-			// put cube
-			addSequential(new PlaceCube());
-
-			// drive backwards
-			addSequential(new DriveArcade(Robot.drivetrain, () -> -FORWARD_SPEED.get(), () -> 0.0),
-					FORWARD_TIME_OUT.get());
-
-			// rotate 180 degrees
-			addSequential(
-					new DriveArcade(Robot.drivetrain, () -> 0.0,
-							() -> startSide == 'L' ? TURNING_SPEED.get() : -TURNING_SPEED.get()),
-					TURNING_TIME_OUT.get() * 2);
-
-			// initiate picking cube sequence
-			addParallel(new PickUpCube());
-
-			// chase the cube
-			addSequential(new DriveArcadeWithPID(Robot.drivetrain, ImageProcessingConstants.CENTER, () -> 0.0,
-					ORIENTATION_FORWARD_SPEED, () -> SubsystemComponents.Roller.hasCube(),
-					new PIDSettings(SubsystemConstants.Drivetrain.ORIENTATION_KP.get(),
-							SubsystemConstants.Drivetrain.ORIENTATION_KI.get(),
-							SubsystemConstants.Drivetrain.ORIENTATION_KD.get(), TOLERANCE.get(), PID_WAIT_TIME.get()),
-					ImageProcessingConstants.RANGE));
-
-		}
-		// the wrong side
-		else
-			new FastYetStupidScoreScaleAuto(gameData, startSide).start();
 	}
 }
