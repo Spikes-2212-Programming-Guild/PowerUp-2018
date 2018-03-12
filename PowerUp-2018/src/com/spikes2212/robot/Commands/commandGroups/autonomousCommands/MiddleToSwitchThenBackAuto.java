9b883fdf12269f6b8408c40dc9b65bc602426ce4
Spikes-2212 <spikes.no2212@gmail.com>
2018-03-12 09:34:06 +0200

    added MiddleToSwitchAuto

---
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/MiddleToSwitchThenBackAuto.java
@@ -0,0 +1,25 @@
+package com.spikes2212.robot.Commands.commandGroups.autonomousCommands;
+
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+import com.spikes2212.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class MiddleToSwitchThenBackAuto extends CommandGroup {
+	public static Supplier<Double> BACK_SPEED = ConstantHandler.addConstantDouble("middle then back - back speed",
+			-0.3);
+	public static Supplier<Double> BACK_TIME = ConstantHandler.addConstantDouble("middle then back - back time", 0.5);
+
+	public MiddleToSwitchThenBackAuto() {
+		// drive back
+		addSequential(new DriveArcade(Robot.drivetrain, BACK_SPEED, () -> 0.0), BACK_TIME.get());
+
+		// rotate to cube
+		addSequential(new DriveArcade(Robot.drivetrain, () -> 0.0, ScoreScale.CloseScale.TURNING_SPEED),
+				ScoreScale.CloseScale.TURNING_TIME_OUT.get());
+	}
+
+}
