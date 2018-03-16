b98ef1c50dd9695163c1b1f604312cf56af4d094
Tomer Sitton <tomer.sitton1@gmail.com>
2018-03-16 16:35:00 +0200

    renamed names in constant handler

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/MoveToSetpointWithEncoders.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/MoveToSetpointWithEncoders.java
@@ -16,17 +16,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MoveToSetpointWithEncoders extends CommandGroup {
 
 	// defining PID constants
-	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("move to switch - tolerance",
+	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("move with encoders - tolerance",
 			10);
 	public static final Supplier<Double> PID_WAIT_TIME = ConstantHandler
-			.addConstantDouble("move to switch - pid wait time", 1);
+			.addConstantDouble("move with encoders - pid wait time", 1);
 
-	public static final Supplier<Double> DRIVING_KP = ConstantHandler.addConstantDouble("move to switch - driving kp",
-			0.004);
-	public static final Supplier<Double> DRIVING_KI = ConstantHandler.addConstantDouble("move to switch - driving ki",
-			0.000025);
-	public static final Supplier<Double> DRIVING_KD = ConstantHandler.addConstantDouble("move to switch - driving kd",
-			0.0);
+	public static final Supplier<Double> DRIVING_KP = ConstantHandler
+			.addConstantDouble("move with encoders - driving kp", 0.004);
+	public static final Supplier<Double> DRIVING_KI = ConstantHandler
+			.addConstantDouble("move with encoders - driving ki", 0.000025);
+	public static final Supplier<Double> DRIVING_KD = ConstantHandler
+			.addConstantDouble("move with encoders - driving kd", 0.0);
 
 	public MoveToSetpointWithEncoders(Supplier<Double> setPoint) {
 		PIDSettings settings = new PIDSettings(DRIVING_KP.get(), DRIVING_KI.get(), DRIVING_KD.get(), TOLERANCE.get(),
