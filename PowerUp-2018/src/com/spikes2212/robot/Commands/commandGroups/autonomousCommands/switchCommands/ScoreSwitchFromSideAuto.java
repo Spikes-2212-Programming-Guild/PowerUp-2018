558bca3d091ad658f1c5ebd04ca1575d9bcc3e6d
Tomer Sitton <tomer.sitton1@gmail.com>
2018-03-16 17:01:28 +0200

    made ScoreSwitchFromSideAuto use turnWithEncoders

--- PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/switchCommands/ScoreSwitchFromSideAuto.java
+++ PowerUp-2018/src/com/spikes2212/robot/Commands/commandGroups/autonomousCommands/switchCommands/ScoreSwitchFromSideAuto.java
@@ -8,6 +8,7 @@ import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
 import com.spikes2212.robot.Robot;
 import com.spikes2212.robot.SubsystemComponents;
 import com.spikes2212.robot.SubsystemConstants;
+import com.spikes2212.robot.Commands.TurnWithEncoders;
 import com.spikes2212.robot.Commands.commandGroups.MoveLiftToTarget;
 import com.spikes2212.robot.Commands.commandGroups.PlaceCube;
 import com.spikes2212.robot.Commands.commandGroups.autonomousCommands.MoveToSetpointWithEncoders;
@@ -17,14 +18,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ScoreSwitchFromSideAuto extends CommandGroup {
 
 	// defining PID set point of the switch
-		public static final Supplier<Double> SWITCH_SET_POINT = ConstantHandler
-				.addConstantDouble("move to switch - switch set point", 135);
-	
+	public static final Supplier<Double> SWITCH_SET_POINT = ConstantHandler
+			.addConstantDouble("move to switch - switch set point", 135);
+
 	// defining turning constants
-	public static final Supplier<Double> TURNING_SPEED = ConstantHandler
-			.addConstantDouble("score switch from side auto - turning speed", 0.5);
-	public static final Supplier<Double> TURNING_TIME_OUT = ConstantHandler
-			.addConstantDouble("score switch from side auto - turning timeout", 1.2);
+	// public static final Supplier<Double> TURNING_SPEED = ConstantHandler
+	// .addConstantDouble("score switch from side auto - turning speed", 0.5);
+	// public static final Supplier<Double> TURNING_TIME_OUT = ConstantHandler
+	// .addConstantDouble("score switch from side auto - turning timeout", 1.2);
 
 	public static final Supplier<Double> FORWARD_SPEED = ConstantHandler
 			.addConstantDouble("score switch from side auto - forward speed", 0.4);
@@ -39,8 +40,10 @@ public class ScoreSwitchFromSideAuto extends CommandGroup {
 		addSequential(new MoveToSetpointWithEncoders(SWITCH_SET_POINT));
 
 		// turning towards the switch
-		addSequential(new DriveArcade(Robot.drivetrain, () -> 0.0,
-				() -> startSide == 'L' ? -TURNING_SPEED.get() : TURNING_SPEED.get()), TURNING_TIME_OUT.get());
+		// addSequential(new DriveArcade(Robot.drivetrain, () -> 0.0,
+		// () -> startSide == 'L' ? -TURNING_SPEED.get() : TURNING_SPEED.get()),
+		// TURNING_TIME_OUT.get());
+		addSequential(new TurnWithEncoders(startSide == 'L' ? -90 : 90));
 
 		// moving to the switch and placing the cube
 		addSequential(new DriveArcade(Robot.drivetrain, FORWARD_SPEED, () -> 0.0), FORWARD_TIME_OUT.get());
