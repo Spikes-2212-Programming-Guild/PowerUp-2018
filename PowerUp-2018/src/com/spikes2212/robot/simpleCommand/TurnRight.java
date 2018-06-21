253933fb2856e7eb8407dd532cb2fb1bcd77a2bb
Matangn13 <matangn13@gmail.com>
2018-06-21 14:53:27 +0300

    Changed TurnRight so it will be more advanced for the hamshushim

--- PowerUp-2018/src/com/spikes2212/robot/simpleCommand/TurnRight.java
+++ PowerUp-2018/src/com/spikes2212/robot/simpleCommand/TurnRight.java
@@ -5,7 +5,7 @@ import com.spikes2212.robot.Robot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class TurnRight extends CommandGroup{
-	public TurnRight() {
-		addSequential(new TurnWithIMU(Robot.drivetrain, ()->-TurnWithIMU.ROTATE_SPEED.get(), -90.0, TurnWithIMU.ROTATE_TOLERANCE.get()));
+	public TurnRight(double angle) {
+		addSequential(new TurnWithIMU(Robot.drivetrain, ()->-TurnWithIMU.ROTATE_SPEED.get(), -angle, TurnWithIMU.ROTATE_TOLERANCE.get()));
 	}
 }
