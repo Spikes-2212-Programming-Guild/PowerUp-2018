b95e9da9a25d468abbe459d6887f033b01f1c914
Matangn13 <matangn13@gmail.com>
2018-06-21 14:53:11 +0300

    Changed TurnLeft so it will be more advanced for the hamshushim

--- PowerUp-2018/src/com/spikes2212/robot/Robot.java
+++ PowerUp-2018/src/com/spikes2212/robot/Robot.java
@@ -24,7 +24,6 @@ import com.spikes2212.robot.Commands.commandGroups.autonomousCommands.PassAutoLi
 import com.spikes2212.robot.Commands.commandGroups.autonomousCommands.ScoreCloseScaleByTime;
 import com.spikes2212.robot.Commands.commandGroups.autonomousCommands.ScoreSwitchFromSideAuto;
 import com.spikes2212.robot.Commands.commandGroups.autonomousCommands.StraightToSwitchAuto;
-import com.spikes2212.robot.simpleCommand.Move;
 import com.spikes2212.robot.simpleCommand.MoveLiftDown;
 import com.spikes2212.robot.simpleCommand.MoveLiftUp;
 import com.spikes2212.robot.simpleCommand.TurnLeft;
@@ -189,11 +188,10 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putData("start side chooser", startSideChooser);
 		
 		//simple commands
-		SmartDashboard.putData("Turn left", new TurnLeft());
-		SmartDashboard.putData("Turn right", new TurnRight());
+		SmartDashboard.putData("Turn left", new TurnLeft(90));
+		SmartDashboard.putData("Turn right", new TurnRight(90));
 		SmartDashboard.putData("Move lift up", new MoveLiftUp());
 		SmartDashboard.putData("Move lift down", new MoveLiftDown());
-		SmartDashboard.putData("Move", new Move(0.5));
 		SmartDashboard.putData("Place cube", new PlaceCube());
 
 
--- PowerUp-2018/src/com/spikes2212/robot/simpleCommand/TurnLeft.java
+++ PowerUp-2018/src/com/spikes2212/robot/simpleCommand/TurnLeft.java
@@ -5,7 +5,7 @@ import com.spikes2212.robot.Robot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class TurnLeft extends CommandGroup {
-	public TurnLeft() {
-		addSequential(new TurnWithIMU(Robot.drivetrain, TurnWithIMU.ROTATE_SPEED, 90.0, TurnWithIMU.ROTATE_TOLERANCE.get()));
+	public TurnLeft(double angle) {
+		addSequential(new TurnWithIMU(Robot.drivetrain, TurnWithIMU.ROTATE_SPEED,angle , TurnWithIMU.ROTATE_TOLERANCE.get()));
 	}
 }
