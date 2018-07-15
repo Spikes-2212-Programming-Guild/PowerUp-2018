e4a3790d73c6a0a822d169dfadac4ffb7eae1fe8
Matangn13 <matangn13@gmail.com>
2018-07-15 15:24:38 +0300

    Added the command group for green

--- PowerUp-2018/src/com/spikes2212/robot/simpleCommand/Green.java
+++ PowerUp-2018/src/com/spikes2212/robot/simpleCommand/Green.java
@@ -8,6 +8,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Green extends CommandGroup {
 
     public Green() {
-    	addSequential();
+    	addSequential(new Drive(1,1,1));
+    	addSequential(new TurnLeft(45));
+    	addSequential(new Drive(1,1,0.5));
+    	addSequential(new TurnRight(90));
+    	addSequential(new Drive(1,1,0.5));
+    	addSequential(new TurnLeft(45));
+    	addSequential(new Drive(1,1,3));
+    	addSequential(new TurnRight(90));
+    	addSequential(new Drive(1,1,1.5));
+    	
     }
 }
