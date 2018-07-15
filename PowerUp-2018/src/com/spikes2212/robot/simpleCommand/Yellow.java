77d074f3ffc44125d9fa34a6511af373d7a7d32f
Itamar Rivkind <Michorn313@gmail.com>
2018-07-15 16:31:22 +0300

    2

--- PowerUp-2018/src/com/spikes2212/robot/simpleCommand/Yellow.java
+++ PowerUp-2018/src/com/spikes2212/robot/simpleCommand/Yellow.java
@@ -10,12 +10,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Yellow extends CommandGroup {
 
     public Yellow() {
-      addSequential(new Drive(0.5,0.5,2));
+      addSequential(new Drive(0.6,0.5,2));
       addSequential(new TurnLeft(45));
-      addSequential(new Drive(0.5,0.5,1));
+      addSequential(new Drive(0.6,0.5,1));
       addSequential(new Drive(0.4,0.5,1));
       addSequential(new TurnLeft(45));
-      addSequential(new Drive(0.5,0.5,2));
+      addSequential(new Drive(0.6,0.5,2));
       
     }
 }
