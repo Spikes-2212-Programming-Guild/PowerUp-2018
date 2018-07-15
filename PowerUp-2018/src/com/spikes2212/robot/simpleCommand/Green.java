069daeea84aed124f2897439f725121ba26ae3da
Matangn13 <matangn13@gmail.com>
2018-07-15 14:27:23 +0300

    Added the simple command group - green

---
+++ PowerUp-2018/src/com/spikes2212/robot/simpleCommand/Green.java
@@ -0,0 +1,13 @@
+package com.spikes2212.robot.simpleCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Green extends CommandGroup {
+
+    public Green() {
+    	addSequential();
+    }
+}
